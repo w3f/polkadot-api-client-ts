36da13b9f6fa0d318ab867e71e41802206e596aa
Federico Gimenez <federico.gimenez@gmail.com>
2020-04-07 14:20:32 +0200

    add transaction test

--- test/client.ts
+++ test/client.ts
@@ -1,9 +1,13 @@
+import { Balance } from '@polkadot/types/interfaces';
+import BN from 'bn.js';
+import fs from 'fs-extra';
 import { Keyring } from '@polkadot/api';
-import { TestRPC } from '@w3f/test-utils';
-
 import { should } from 'chai';
+import { TestRPC } from '@w3f/test-utils';
+import tmp from 'tmp';
 
 import { Client } from '../src/client';
+import { Keystore } from '../src/types';
 import { ZeroBalance } from '../src/constants';
 
 should();
@@ -12,7 +16,8 @@ const testRPC = new TestRPC();
 const subject = new Client(testRPC.endpoint());
 let keyring: Keyring;
 
-describe('TestRPC', () => {
+
+describe('Client', () => {
     before(async () => {
         await testRPC.start();
         keyring = new Keyring({ type: 'sr25519' });
@@ -28,5 +33,28 @@ describe('TestRPC', () => {
         const balance = await subject.balanceOf(alice.address);
 
         ZeroBalance.lt(balance).should.be.true;
+    });
+
+    it('should make transfers', async () => {
+        const alice = keyring.addFromUri('//Alice');
+        const bob = keyring.addFromUri('//Bob');
+
+        const initialBobBalance = await subject.balanceOf(bob.address);
+
+        const pass = 'pass';
+        const aliceKeypairJson = keyring.toJson(alice.address, pass);
+        const ksFile = tmp.fileSync();
+        fs.writeSync(ksFile.fd, JSON.stringify(aliceKeypairJson));
+        const passFile = tmp.fileSync();
+        fs.writeSync(passFile.fd, pass);
+
+        const toSend = new BN(10000000000000);
+        const ks: Keystore = { filePath: ksFile.name, passwordPath: passFile.name };
+        await subject.send(ks, bob.address, toSend as Balance);
+
+        const finalBobBalance = await subject.balanceOf(bob.address);
+
+        const expected = (initialBobBalance as BN).add(toSend) as Balance;
+        finalBobBalance.eq(expected).should.be.true;
     });
 });
