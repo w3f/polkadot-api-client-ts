0938149e7d7f0edde7be385b31fca035d2df361a
Federico Gimenez <federico.gimenez@gmail.com>
2020-04-29 17:26:14 +0200

    export more types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './client';
 export * from './constants';
 export * from './types';
+
+export { ApiPromise, Keyring } from '@polkadot/api';
+export { Balance } from '@polkadot/types/interfaces'
