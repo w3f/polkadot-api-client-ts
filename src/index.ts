00d6a8e331797826510942d2292148111e8ebd0a
Federico Gimenez <federico.gimenez@gmail.com>
2020-05-12 19:18:53 +0200

    bump deps

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from './constants';
 export * from './types';
 
 export { ApiPromise, Keyring } from '@polkadot/api';
-export { Balance } from '@polkadot/types/interfaces'
+export { Balance } from '@polkadot/types/interfaces';
